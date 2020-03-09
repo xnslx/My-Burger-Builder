9d3f1bc4f2d572ca38432b4db0645432d84fe871
Xian Li <xnslx89@gamil.com>
2020-03-09 14:22:04 -0500

    second change

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,7 +5,8 @@ import classes from './Logo.module.css';
 const logo = (props) => ( <
     div className = { classes.Logo }
     style = {
-        { height: props.height } } >
+        { height: props.height }
+    } >
     <
     img src = { burgerLogo }
     alt = "MyBurger" / >
